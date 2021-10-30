78e6e8d8955d17e25396277bd54b6c840ee6889f
Dinis Costa <61733414+scorpaust@users.noreply.github.com>
2021-10-30 13:05:35 +0100

    Camera controller.
    Camera following player in a clamped position.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private float offsetX = 3f;
+
+    [SerializeField]
+    private BoxCollider2D boundsBox;
+
+    private PlayerController player;
+
+    private float halfHeight, halfWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeVariables();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPlayer();
+
+        ClampCameraPosition();
+    }
+
+    private void InitializeVariables()
+	{
+        player = FindObjectOfType<PlayerController>();
+
+        halfHeight = Camera.main.orthographicSize;
+
+        halfWidth = halfHeight * Camera.main.aspect;
+    }
+
+    private void FollowPlayer()
+	{
+        if (player != null)
+		{
+            transform.position = new Vector3(player.transform.position.x + offsetX, player.transform.position.z, transform.position.z);
+		}
+	}
+
+    private void ClampCameraPosition()
+	{
+        Mathf.Clamp(player.transform.position.x, boundsBox.bounds.min.x + halfWidth, boundsBox.bounds.max.x);
+
+        Mathf.Clamp(player.transform.position.y, boundsBox.bounds.min.y + halfHeight, boundsBox.bounds.max.y);
+    }
+}
